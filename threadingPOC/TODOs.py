3e13e8b25f893d65c0eccf6265a1c44b14cf84b0
Jan Weil <albcycle@gmx.net>
2021-07-26 09:21:11 +0200

    delete old directory - see MultithreadingPoC

--- threadingPOC/TODOs.py
+++
@@ -1,6 +0,0 @@
-# split threading and non threading part into two programs
-# TODO: write bachelor documentation
-# TODO: create two similar sqlite test files (reproducible)
-# TODO: expand test two json file types
-# TODO: (optional) visualize results in a graph to see misfits
-# TODO: create final Development-Roadmap (use concept)
