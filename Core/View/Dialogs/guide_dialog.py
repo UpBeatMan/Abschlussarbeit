d5ab513b60dba4eedc46ae16fcc2ab13ace0f97a
Jan Weil <albcycle@gmx.net>
2021-08-06 09:56:56 +0200

    added comments

--- Core/View/Dialogs/guide_dialog.py
+++ Core/View/Dialogs/guide_dialog.py
@@ -1,9 +1,9 @@
-import tkinter as tk
+import tkinter as tk  # ToolKit module
+from Model.util import resource_path  # get absolute path to temp _MEIPASS location
 
-from Model.util import resource_path
 
-
-class GuideDialog(tk.Toplevel):
+class GuideDialog(tk.Toplevel):  # toplevel - popup confirmation window
+    # * a windows with text and an okay button to close it again
     def __init__(self, parent, controller):
         tk.Toplevel.__init__(self, parent)
         self.title("Nutzerhandbuch")
@@ -11,11 +11,13 @@ class GuideDialog(tk.Toplevel):
 
         self.controller = controller
 
-        self.text_frame = tk.Frame(self, width=130)
-        self.text = tk.Text(self.text_frame, width=130)
-        self.text.config(state=tk.DISABLED)
+        self.text_frame = tk.Frame(self, width=130)  # a frame to organize other widgets
+        self.text = tk.Text(self.text_frame, width=130)  # a text field widget
+        self.text.config(state=tk.DISABLED)  # disabled text widget
+        # organizes widget in blocks before placing them in the parent widget
         self.text.pack()
         self.text_frame.pack(side=tk.TOP)
+        # import text from ./View/text/guide.txt
         self.file_to_text()
 
         button_frame = tk.Frame(self)
@@ -25,16 +27,17 @@ class GuideDialog(tk.Toplevel):
             button_frame, text="OK", width=15, command=self.on_ok
         )
 
-        self.ok_button.pack()
+        self.ok_button.pack()  # organizes widgets in blocks before placing them in the parent widget
 
     def on_ok(self, event=None):
-        self.destroy()
+        self.destroy()  # destroy windows when clicking ok
 
     def show(self):
-        self.wm_deiconify()
-        self.wait_window()
+        self.wm_deiconify()  # displays the window, after using either the iconify or the withdraw methods
+        self.wait_window()  # this method can be called after the event which needs to happen before the window event
 
     def file_to_text(self):
+        # * read text from guide.txt and insert it to the text widget
         text = u""
         self.text.config(state=tk.NORMAL)
         guide = open(resource_path("View/text/guide.txt"), "r")
