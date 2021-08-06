be429212603cd0f1def58a990447600c5265d54f
Jan Weil <albcycle@gmx.net>
2021-08-06 10:08:53 +0200

    added comments

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,7 +1,7 @@
-from Controller import Controller
+from Controller import Controller # import own controller module
 
 __version__ = "0.1.0"
 
 if __name__ == "__main__":
     c = Controller()
-    c.main()
+    c.main()    # start Controller.main function
