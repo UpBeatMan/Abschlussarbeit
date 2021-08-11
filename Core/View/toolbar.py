03aa65116979e74c075940850796f4f2b8bf80db
Jan Weil <albcycle@gmx.net>
2021-08-11 17:40:23 +0200

    major toolbar upgrade: gui changes for ProgressBar and relocation of Buttons, also a new icon set

--- Core/View/toolbar.py
+++ Core/View/toolbar.py
@@ -1,7 +1,13 @@
 import tkinter as tk
+from tkinter.constants import HORIZONTAL
 from PIL import Image, ImageTk  #
-from View.Dialogs.ask_dialog import AskDialog # confirmation popup window
-from Model.util import resource_path # get absolute path to temp _MEIPASS location
+from View.Dialogs.ask_dialog import AskDialog  # confirmation popup window
+from Model.util import resource_path  # get absolute path to temp _MEIPASS location
+
+# from tkinter.ttk import Progressbar, Style
+from tkinter import ttk
+from time import sleep
+from tkinter import Label
 
 
 class Toolbar(tk.Frame):
@@ -9,32 +15,62 @@ class Toolbar(tk.Frame):
         tk.Frame.__init__(self, bd=1, relief=tk.RAISED)
         self.parent = parent
 
+        self.progressBar = None
+        # * where should I put my flag to start this activity progress bar indicator
+        # ! self.load_activity_flag = None
+
         self.body()
 
     def body(self):
 
         img_first = Image.open(resource_path("Core/View/icons/Exit_Icon.png"))
         exit_img = ImageTk.PhotoImage(img_first)
+        exit_img = exit_img._PhotoImage__photo.subsample(15)
 
         img_sec = Image.open(resource_path("Core/View/icons/Rollback_Icon.png"))
         rollback_img = ImageTk.PhotoImage(img_sec)
+        rollback_img = rollback_img._PhotoImage__photo.subsample(15)
 
-        img_third = Image.open(resource_path("Core/View/icons/Safe_Icon.png"))
-        safe_img = ImageTk.PhotoImage(img_third)
+        img_third = Image.open(resource_path("Core/View/icons/Save_Icon.png"))
+        save_img = ImageTk.PhotoImage(img_third)
+        save_img = save_img._PhotoImage__photo.subsample(15)
 
-        #! replace the load button to this location !
+        img_fourth = Image.open(resource_path("Core/View/icons/Reload_Icon.png"))
+        reload_img = ImageTk.PhotoImage(img_fourth)
+        reload_img = reload_img._PhotoImage__photo.subsample(15)
 
+        img_fifth = Image.open(resource_path("Core/View/icons/Load_Icon.png"))
+        load_img = ImageTk.PhotoImage(img_fifth)
+        load_img = load_img._PhotoImage__photo.subsample(15)
+
+        # * changed safeButton to saveButton
+        saveButton = tk.Button(
+            self,
+            image=save_img,
+            relief=tk.FLAT,
+            command=self.parent.controller.commit_all_data,
+        )
+        saveButton.image = save_img
 
         exitButton = tk.Button(self, image=exit_img, relief=tk.FLAT, command=self.quit)
         exitButton.image = exit_img
 
-        safeButton = tk.Button(
+        profileButton = tk.Button(
             self,
-            image=safe_img,
+            image=reload_img,
+            # text="Profile aktualisieren",
             relief=tk.FLAT,
-            command=self.parent.controller.commit_all_data,
+            command=self.parent.sidebar.insert_profiles_to_treeview,
         )
-        safeButton.image = safe_img
+        profileButton.image = reload_img
+
+        loadButton = tk.Button(
+            self,
+            image=load_img,
+            relief=tk.FLAT,
+            command=self.parent.sidebar.load_profile,
+        )
+        loadButton.image = load_img
 
         rollbackButton = tk.Button(
             self,
@@ -43,18 +79,74 @@ class Toolbar(tk.Frame):
             command=self.parent.controller.rollback_all_data,
         )
         rollbackButton.image = rollback_img
-        load_profiles_button = tk.Button(
-            self,
-            text="Profile aktualisieren",
-            relief=tk.FLAT,
-            command=self.parent.sidebar.insert_profiles_to_treeview,
+
+        # # progressbar theme:
+        # theme = ttk.Style()
+        # theme.theme_use("winnative")
+        # theme.configure("green.Horizontal.TProgressbar", background="green")
+
+        # # progressbar:
+        # self.bar = ttk.Progressbar(
+        #     self.parent,
+        #     style="green.Horizontal.TProgressbar",
+        #     orient="horizontal",
+        #     mode="indeterminate",
+        #     length="180",
+        # )
+
+        # # update root to see animation:
+        # self.parent.update()
+        # self.play_animation()
+
+        loading = Label(self, text="Daten werden geladen...", fg="green")
+
+        self.progressBar = ttk.Progressbar(
+            self, orient=HORIZONTAL, length=160, mode="determinate"
         )
 
-        exitButton.pack(side=tk.LEFT, padx=4, pady=2, fill="both")
-        safeButton.pack(side=tk.LEFT, padx=4, pady=2, fill="both")
-        rollbackButton.pack(side=tk.LEFT, padx=4, pady=2, fill="both")
+        saveButton.pack(side=tk.LEFT, padx=4, pady=2)
+        profileButton.pack(side=tk.LEFT, padx=4, pady=2)
+        exitButton.pack(side=tk.LEFT, padx=4, pady=2)
+        loading.pack(side=tk.LEFT, padx=4, pady=2)
+        self.progressBar.pack(side=tk.LEFT, padx=4, pady=1)
+        loadButton.pack(side=tk.LEFT, padx=4, pady=2)
 
-        load_profiles_button.pack(side=tk.LEFT, padx=4, pady=2, fill="both")
+        rollbackButton.pack(side=tk.LEFT, padx=4, pady=2)
+
+        # self.start()
+
+        # while True:
+        #     status = self.get_load_activity_flag()
+        #     if status == True:
+        #         self.start()
+        #     elif status == False:
+        #         self.stop()
+        #     else:
+        #         pass  # error: no valid flag state
+
+    # originally in Config/__init__.pz
+    # def set_load_activity_flag(self, switch: bool):
+    #     self.load_activity_flag = switch
+
+    # def get_load_activity_flag(self):
+    #     status =  self.load_activity_flag
+    #     return status
+
+    def start(self):
+        self.progressBar.start()
+
+    def stop(self):
+        self.progressBar.stop()
+
+    # progressbar animation:
+    # def play_animation(self):
+    #     for i in range(2000):
+    #         self.bar["value"] += 1
+    #         self.parent.update_idletasks()
+    #         sleep(0.01)
+    #     else:
+    #         self.root.destroy()
+    #         exit(0)
 
     def quit(self):
         if self.parent.controller.get_unsaved_handlers():
