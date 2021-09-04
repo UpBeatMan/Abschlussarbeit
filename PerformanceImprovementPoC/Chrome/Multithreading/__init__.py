ef142dab8cbf8964a10cfafa4b2c490e559c952a
Jan Weil <albcycle@gmx.net>
2021-09-04 16:58:24 +0200

    multithreading script fitted for easy usage

--- PerformanceImprovementPoC/Chrome/Multithreading/__init__.py
+++ PerformanceImprovementPoC/Chrome/Multithreading/__init__.py
@@ -2,23 +2,24 @@
 #!-*- coding: utf-8 -*-
 # * for ChromeModel
 
-from threading import Thread
-from statistics import mean
-from timeit import Timer
+import getpass
 import os
 import shutil
 import database
+from threading import Thread
+from timeit import Timer
+from statistics import mean
 
+# version 2.2 test with chrome browser profile with threading
 __version__ = "0.2.2"
 
 # * choose thread count
 TH_LOW: int = 2
 TH_MED: int = 4
 TH_HIG: int = 8
-TH_EXT: int = 16
 
-# ! edit windows user name !
-USERNAME = "Yochanan"
+# get windows user name
+USERNAME = getpass.getuser()
 # * chrome history
 HISTORY = "History"
 
@@ -38,14 +39,13 @@ TEST_FILE = TEST_DIR + "\\History.sqlite"
 
 # table name which is going to be requested
 TABLE = "urls"
-# count_query() result - static test variable
-ROW_COUNT = 107
+# def count_query() result - static test variable
+ROW_COUNT = 4725 # 107
 # first id in table - static test variable
-START_ID: int = 1
+START_ID = 1
 
 
 def main():
-
     # ! Comment out for testing !
     # print(f"{FILE_HANDLER} _ target file")
     # if os.path.exists(TEST_DIR):
@@ -70,24 +70,26 @@ def main():
         query = build.format(TBL_HST=TABLE)
         dbms.print_all_data(query=query)
 
-    def threaded_testcase(iterations_i, result_list, th_cat):
+    def threaded_testcase(iterations_i, result_list, threads):
         for loop in range(iterations_i):
             # with threading module
-            t = Timer(lambda: calc_borders(START_ID, ROW_COUNT, th_cat))
+            t = Timer(lambda: calc_borders(START_ID, ROW_COUNT, threads))
             time = t.timeit(number=1)
             # print(
-            #     f"Zeitmessung mit {th_cat} Threads in ns - Durchlauf {loop + 1}: {time}"
+            #     f"Zeitmessung mit {threads} Threads in ns - Durchlauf {loop + 1}: {time}"
             # )
             result_list.append(time)
         return result_list
 
-    def show_results(iterations_i, result_list):
+    def show_results(iterations_i, result_list, threads):
         # print("\n")
         # print(result_list)
         mean_value = mean(result_list)
         # plain_val = "%f" % mean_value
         # print(plain_val)
-        print(f"Durchschnittswert über {iterations_i} Testläufe: {mean_value}\n")
+        print(
+            f"Durchschnitts-Zeitwert über {iterations_i} Testläufe mit {threads} Threads: {mean_value}\n"
+        )
 
     # calculate table row ranges according to the thread numbers
     def calc_borders(entry, rows, threads):
@@ -101,30 +103,36 @@ def main():
             t.start()
             last_entry = next_border + 1
 
-    def each_thread(rows, th_cat):
-        # Es wird nach unten gerundet. Für den Test vorerst irrelevant!
-        each_th = rows // th_cat
-        print(f"With {th_cat} each thread handles {each_th}.")
+    def each_thread(rows, threads):
+        # prevent reading more table lines than existing
+        # with the floor division operator, rounding
+        # float values to the next lower integer
+        each_th = rows // threads
+        print(f"With {threads} each thread handles {each_th}.")
         return each_th
 
     # print("Choose a desired thread count")
     # each_thread(ROW_COUNT, TH_LOW)
     # each_thread(ROW_COUNT, TH_MED)
     # each_thread(ROW_COUNT, TH_HIG)
-    # each_thread(ROW_COUNT, TH_EXT)
 
-    # show table content - activate print(row) in database.py
-    # important - deactivate test functions below!
-    # dbms.print_all_data(database.TABLE)
+    # important - deactivate test functions below line 129!
+    # activate prints for count table and query content
+    # in function def print_all_data in database.py
+    #dbms.count_query()
+    # activate prints for query content and more query content
+    # in function def print_all_data in database.py
+    #dbms.print_all_data(database.TABLE)
 
     # *Zeitmessung mit Threading und Aufteilen der Lesezugriffe
 
     results_threaded = []
     i = 800
+    # change TH_<priority> to LOW or MED or HIG
+    th_cat = TH_LOW
 
-    # ! change TH_<priority> to LOW, MED, HIG, EXT !
-    threaded_testcase(i, results_threaded, TH_LOW)
-    show_results(i, results_threaded)
+    threaded_testcase(i, results_threaded, th_cat)
+    show_results(i, results_threaded, th_cat)
 
 
 # run the program
--- PerformanceImprovementPoC/Chrome/Multithreading/database.py
+++ PerformanceImprovementPoC/Chrome/Multithreading/database.py
@@ -2,13 +2,15 @@
 #!-*- coding: utf-8 -*-
 # * for ChromeModel
 
+import getpass
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-__version__ = "0.1.2"
+# database module version 2.3 with getpass and for chrome data structure
+__version__ = "0.2.3"
 
-# ! edit windows user name !
-USERNAME = "Yochanan"
+# get windows user name
+USERNAME = getpass.getuser()
 # * database type
 SQLITE = "sqlite"
 # * chrome history
@@ -19,13 +21,10 @@ TABLE = "urls"
 class MyDatabase:
     # http://docs.sqlalchemy.org/en/latest/core/engines.html
     DB_ENGINE = {
-        SQLITE: SQLITE
-        + ":///C:\\Users\\"
+        SQLITE: SQLITE + ":///C:\\Users\\"
         + USERNAME
-        + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\SINGLETHREADING\\"
-        + HISTORY
-        + "."
-        + SQLITE
+        + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\MULTITHREADING\\"
+        + HISTORY + "." + SQLITE
     }
     # main db connection reference object
     db_engine = None
@@ -44,8 +43,8 @@ class MyDatabase:
     # show all data from table
     def print_all_data(self, table="", query=""):
         query = query if query != "" else "SELECT * FROM '{}';".format(table)
-        # output switch
-        # print(query)
+        # output switch - count table
+        #print(query)
         with self.db_engine.connect() as connection:
             try:
                 result = connection.execute(query)
@@ -53,15 +52,15 @@ class MyDatabase:
                 print(e)
             else:
                 for row in result:
-                    # output switch
-                    print(row)
+                    # output switch - query content
+                    #print(row)
                     pass
-                # print("\n")
+                # output switch - more query content
+                #print("\n")
                 result.close()
 
-    # Optional count query
-    # def count_query(self):
-    #     # sample query for testing
-    #     # print("\n")
-    #     query = "SELECT count(id) FROM {TBL_HST};".format(TBL_HST=TABLE)
-    #     self.print_all_data(query=query)
+    # Optional count query - activate in multithreading.py
+    def count_query(self):
+        # sample query for testing
+        query = "SELECT count(id) FROM {TBL_HST};".format(TBL_HST=TABLE)
+        self.print_all_data(query=query)
