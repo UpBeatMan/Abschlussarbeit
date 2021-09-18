9b8e4c2ac8cb0ca5a304d3be94574e347b02cc47
Jan Weil <albcycle@gmx.net>
2021-09-18 14:23:20 +0200

    new monitor_activity function for loading activity

--- Core/View/toolbar.py
+++ Core/View/toolbar.py
@@ -1,27 +1,27 @@
+# import tkinter gui modules
 import tkinter as tk
-from tkinter.constants import HORIZONTAL
-from PIL import Image, ImageTk  #
-from View.Dialogs.ask_dialog import AskDialog  # confirmation popup window
-from Model.util import resource_path  # get absolute path to temp _MEIPASS location
-
-# from tkinter.ttk import Progressbar, Style
 from tkinter import ttk
-from time import sleep
-from tkinter import Label
+from PIL import Image, ImageTk  # use images in tkinter module
+
+# import dialog function from View.Dialogs.ask_dialog
+from View.Dialogs.ask_dialog import AskDialog  # confirmation popup window
+
+# import utility functions from Model.util
+from Model.util import resource_path  # get absolute path to temp _MEIPASS location
+from Model.util import (
+    log_message,
+)  # sends log message to log listener in controller module
 
 
 class Toolbar(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, bd=1, relief=tk.RAISED)
         self.parent = parent
-
         self.progressBar = None
-        # * where should I put my flag to start this activity progress bar indicator
-        # ! self.load_activity_flag = None
-
         self.body()
 
     def body(self):
+        """defines gui elements in the toolbar gui section and their function calls"""
 
         img_first = Image.open(resource_path("Core/View/icons/Exit_Icon.png"))
         exit_img = ImageTk.PhotoImage(img_first)
@@ -43,7 +43,8 @@ class Toolbar(tk.Frame):
         load_img = ImageTk.PhotoImage(img_fifth)
         load_img = load_img._PhotoImage__photo.subsample(15)
 
-        # * changed safeButton to saveButton
+        # * save profile changes button configuration
+        # changed safeButton to saveButton
         saveButton = tk.Button(
             self,
             image=save_img,
@@ -52,18 +53,25 @@ class Toolbar(tk.Frame):
         )
         saveButton.image = save_img
 
-        exitButton = tk.Button(self, image=exit_img, relief=tk.FLAT, command=self.quit)
-        exitButton.image = exit_img
-
+        # * refresh profile tree view button configuration
         profileButton = tk.Button(
             self,
             image=reload_img,
-            # text="Profile aktualisieren",
             relief=tk.FLAT,
             command=self.parent.sidebar.insert_profiles_to_treeview,
         )
         profileButton.image = reload_img
 
+        # * exit application button configuration
+        exitButton = tk.Button(self, image=exit_img, relief=tk.FLAT, command=self.quit)
+        exitButton.image = exit_img
+
+        # * activity indicator configuration
+        self.progressBar = ttk.Progressbar(
+            self, orient="horizontal", length=130, maximum=30, mode="determinate"
+        )
+
+        # * load profile button configuration
         loadButton = tk.Button(
             self,
             image=load_img,
@@ -72,6 +80,7 @@ class Toolbar(tk.Frame):
         )
         loadButton.image = load_img
 
+        # * rollback button configuration
         rollbackButton = tk.Button(
             self,
             image=rollback_img,
@@ -80,75 +89,28 @@ class Toolbar(tk.Frame):
         )
         rollbackButton.image = rollback_img
 
-        # # progressbar theme:
-        # theme = ttk.Style()
-        # theme.theme_use("winnative")
-        # theme.configure("green.Horizontal.TProgressbar", background="green")
-
-        # # progressbar:
-        # self.bar = ttk.Progressbar(
-        #     self.parent,
-        #     style="green.Horizontal.TProgressbar",
-        #     orient="horizontal",
-        #     mode="indeterminate",
-        #     length="180",
-        # )
-
-        # # update root to see animation:
-        # self.parent.update()
-        # self.play_animation()
-
-        loading = Label(self, text="Daten werden geladen...", fg="green")
-
-        self.progressBar = ttk.Progressbar(
-            self, orient=HORIZONTAL, length=160, mode="determinate"
-        )
-
+        # * pack gui elements - determines postitions in relation to each other
         saveButton.pack(side=tk.LEFT, padx=4, pady=2)
         profileButton.pack(side=tk.LEFT, padx=4, pady=2)
         exitButton.pack(side=tk.LEFT, padx=4, pady=2)
-        loading.pack(side=tk.LEFT, padx=4, pady=2)
         self.progressBar.pack(side=tk.LEFT, padx=4, pady=1)
         loadButton.pack(side=tk.LEFT, padx=4, pady=2)
-
         rollbackButton.pack(side=tk.LEFT, padx=4, pady=2)
 
-        # self.start()
-
-        # while True:
-        #     status = self.get_load_activity_flag()
-        #     if status == True:
-        #         self.start()
-        #     elif status == False:
-        #         self.stop()
-        #     else:
-        #         pass  # error: no valid flag state
-
-    # originally in Config/__init__.pz
-    # def set_load_activity_flag(self, switch: bool):
-    #     self.load_activity_flag = switch
-
-    # def get_load_activity_flag(self):
-    #     status =  self.load_activity_flag
-    #     return status
-
-    def start(self):
-        self.progressBar.start()
-
-    def stop(self):
-        self.progressBar.stop()
-
-    # progressbar animation:
-    # def play_animation(self):
-    #     for i in range(2000):
-    #         self.bar["value"] += 1
-    #         self.parent.update_idletasks()
-    #         sleep(0.01)
-    #     else:
-    #         self.root.destroy()
-    #         exit(0)
+    def monitor_activity(self, flag):
+        """handles progessbar indication and log messages for the profile loading"""
+        if flag == "True":
+            self.progressBar["value"] = 100
+            log_message("Ladevorgang begonnen!", lvl="info")
+        elif flag == "False":
+            self.progressBar["value"] = 0
+            log_message("Ladevorgang abgeschlossen!", lvl="info")
+        else:
+            log_message("Unbekannter Status des Ladevorgangs erkannt!", lvl="error")
+            pass
 
     def quit(self):
+        """handles application exit and pre-exit confirmation"""
         if self.parent.controller.get_unsaved_handlers():
             answer = AskDialog(
                 self.parent,
