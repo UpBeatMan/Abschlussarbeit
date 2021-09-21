e365e58fc1976a5f5db956ec8e168d0276bb3a50
Jan Weil <albcycle@gmx.net>
2021-09-21 09:54:39 +0200

    AskDialog unified comment structure

--- Core/View/Dialogs/ask_dialog.py
+++ Core/View/Dialogs/ask_dialog.py
@@ -1,11 +1,15 @@
-import tkinter as tk  # ToolKit module
+# ToolKit module
+import tkinter as tk
 
 
-class AskDialog(tk.Toplevel):  # toplevel - popup confirmation window
+class AskDialog(tk.Toplevel):
+    """AskDialog class opens a popup window"""
+
+    # * toplevel - popup confirmation window
     # * a window with text for a question/request, an okay and cancel button
     def __init__(self, parent, controller, text):
         tk.Toplevel.__init__(self, parent)
-        self.title("Achtung!")
+        self.title("Achtung")
         self.resizable(0, 0)
 
         self.controller = controller
@@ -13,32 +17,34 @@ class AskDialog(tk.Toplevel):  # toplevel - popup confirmation window
 
         tk.Label(self, text=text).pack(side=tk.TOP, pady=10, padx=10)
 
-        # button area
+        # * button area
         button_frame = tk.Frame(self)
         button_frame.pack(side=tk.BOTTOM, pady=6)
 
-        # define buttons
+        # * button configuration
         self.ok_button = tk.Button(
             button_frame, text="OK", width=10, command=self.on_ok
         )
-
-        self.cancel_button = tk.Button( # * changed cancle to cancel
+        self.cancel_button = tk.Button(  # changed from cancle to cancel
             button_frame, text="Abbrechen", width=10, command=self.on_cancel
         )
 
+        # * place(pack) buttons in gui area
         self.ok_button.pack(side=tk.LEFT, padx=5)
-        self.cancel_button.pack(side=tk.RIGHT, padx=5) # * changed cancle to cancel
+        self.cancel_button.pack(side=tk.RIGHT, padx=5)  # changed from cancle to cancel
 
     def on_ok(self, event=None):
+        """on_ok indicates a positive user decision and closes window"""
         self.return_value = True
-        self.destroy() # destroy windows when clicking ok
+        self.destroy()
 
-    def on_cancel(self, event=None): # * changed cancle to cancel
+    def on_cancel(self, event=None):  # changed from cancle to cancel!
+        """on_cancel indicates a negative user decision and closes window"""
         self.return_value = False
-        self.destroy() # destroy windows when clicking ok
+        self.destroy()
 
     def show(self):
-        # * show a hidden tk widget again
+        """shows a hidden tk widget again"""
         self.wm_deiconify()  # displays the window, after using either the iconify or the withdraw methods
         self.wait_window()  # this method can be called after the event which needs to happen before the window event
         return self.return_value
