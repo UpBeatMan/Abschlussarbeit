aaeda9cab0221cf007585a64ca459e277be5a41b
Jan Weil <albcycle@gmx.net>
2021-10-03 15:17:07 +0200

    version definition -  DFSC is 0.2.0

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,6 +1,8 @@
 from Controller import Controller # import own controller module
 
-__version__ = "0.1.0"
+# New updated version: DFSC - Digital Forensics Scenario Creator by Jan Weil is version 0.2.0
+# FiProTima - (Firefox) Browser Profile Timestamp Manipulator by Marco Hirth is version 0.1.0
+__version__ = "0.2.0"
 
 if __name__ == "__main__":
     c = Controller()
